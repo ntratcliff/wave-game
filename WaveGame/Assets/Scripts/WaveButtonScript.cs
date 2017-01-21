3a83f655a4938c7e0b5b3131e05d93f9ec094b0a
Robert <emailrobertb@gmail.com>
2017-01-21 15:19:07 -0500

    Placeholder valeus with hard coding.

--- WaveGame/Assets/Scripts/WaveButtonScript.cs
+++ WaveGame/Assets/Scripts/WaveButtonScript.cs
@@ -80,13 +80,13 @@ public class WaveButtonScript : MonoBehaviour {
                     this.transform.localScale += new Vector3(0.01f, 0, 0.01f);
                 }
 
-                water.AddForce(inputForceP1, water.NumNodes / 3);
+                water.AddForce(inputForceP1 / 2, 5);//water.NumNodes / 3);
             }
-            else if(Input.GetKeyUp(KeyCode.LeftAlt))
+            else
             {
                 increaseForce = false;
                 decreaseForce = true;
-                inputForceP1 -= scaleForce* Time.deltaTime;
+                inputForceP1 = 0; //-= scaleForce* Time.deltaTime;
                 if (inputForceP1 < minCap)
                 {
                     inputForceP1 = minCap;
@@ -115,14 +115,14 @@ public class WaveButtonScript : MonoBehaviour {
 
                 }
 
-                water.AddForce(inputForceP2, water.NumNodes / 3 * 2);
+                water.AddForce(inputForceP2 / 2, 45);// water.NumNodes / 3 * 2);
             }
-            else if(Input.GetKeyUp(KeyCode.RightAlt))
+            else 
             {
                 Debug.Log("Adding the force");
                 increaseForce = false;
                 decreaseForce = true;
-                inputForceP2-= scaleForce* Time.deltaTime;
+                inputForceP2 = 0;//-= scaleForce * Time.deltaTime;
                 if (inputForceP2 < minCap)
                 {
                     inputForceP2 = minCap;
