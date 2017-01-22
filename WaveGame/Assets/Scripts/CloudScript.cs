7de99a7013a31b3a07ecf78f751b0138c449ad10
Robert <emailrobertb@gmail.com>
2017-01-21 22:39:26 -0500

    Clouds all up in this

---
+++ WaveGame/Assets/Scripts/CloudScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+
+    public float y;
+    public float xMod;
+    public float vanillaSpeed;
+    public float speed;
+    //    public float lowerRangeX; //-3 to 3 seems to be the best current one
+    //   public float higherRangeX;
+    //    public float lowerRangeSpeed;
+    //    public float higherRangeSpeed; //recommmend to cap this at 4.
+    //public float speed;
+
+    public float perlinFreq;
+    //private Scoreboard scoreboard;
+    public bool isMoving;
+    //public bool didCollide;
+
+    public float noiseModifierRangeLow; //-2 default
+    public float noiseModifierRangeHigh; //2 default
+    public float heightMultiplier; //4 default
+
+    public float scaleModMin; //.25f
+    public float scaleModMax; //1.33f
+    public float speedModMin; //.1f
+    public float speedMonMax; //.3f
+    public float zAnchor;
+    public float zMod;
+    public float minHeight;
+    public float maxHeight;
+    public float minXmod;
+    public float maxXmod;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //        scoreboard = FindObjectOfType<Scoreboard>();
+        //speed = Random.Range(lowerRangeSpeed, higherRangeSpeed);
+        y = Random.Range(minHeight, maxHeight);//minHeight + (Mathf.PerlinNoise(Time.time + Random.Range(noiseModifierRangeLow, noiseModifierRangeHigh), 0)) * (heightMultiplier - maxHeight);
+        xMod = Random.Range(minXmod, maxXmod);
+        transform.position = new Vector3(transform.position.x + xMod, y, transform.position.z);
+        speed = vanillaSpeed;
+  //      didCollide = false;
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            Vector3 speedAdjust = new Vector3(speed * Time.deltaTime, 0, 0);
+            //    y = Mathf.PerlinNoise(Time.time, 0) * 3;
+            //   transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            transform.position -= speedAdjust;
+        }
+
+        //need way to mess with 
+    }
+
+    public void rerollHeight()
+    {
+        y = Mathf.PerlinNoise(Time.time + Random.Range(noiseModifierRangeLow, noiseModifierRangeHigh), 0) * heightMultiplier;
+    }
+
+    public void rerollScale()
+    {
+        float scaleBy = Random.Range(scaleModMin, scaleModMax);
+        transform.localScale = new Vector3(scaleBy, scaleBy, scaleBy);
+
+        float zPos = zAnchor * (zMod * scaleBy);
+        transform.position = new Vector3(transform.position.x, zPos, transform.position.z);
+
+        speed = vanillaSpeed * scaleBy + Random.Range(speedModMin,speedMonMax);
+    }
+
+
+
+
+}
--- WaveGame/Assets/Scripts/GameManagerScript.cs
+++ WaveGame/Assets/Scripts/GameManagerScript.cs
@@ -8,15 +8,25 @@ public class GameManagerScript : MonoBehaviour {
     public GameObject player2;
     public GameObject audioManager;
     public GameObject collectibleGod;
+    public GameObject cloudGod;
 
     public float lowerRangeSeconds;
     public float higherRangeSeconds;
+    public float lowerCloudRangeSeconds;
+    public float higherCloudRangeSeconds;
 
     private float timeCounter;
     public float sendNextTime;
     public int collCounter;
+
+    public int cloudCounter;
+    private float cloudTimeCounter;
+    public float cloudSendNextTime;
+
     public List<GameObject> collectibles;
+    public List<GameObject> clouds;
     private CollectibleScript cScript;
+    private CloudScript cloudScript;
     private Vector3 newStart;
 	// Use this for initialization
 	void Start () {
@@ -32,8 +42,53 @@ public class GameManagerScript : MonoBehaviour {
         collectibles.Add(c4);
         collectibles.Add(c5);
 
+        GameObject cl1 = Instantiate(cloudGod);
+        GameObject cl2 = Instantiate(cloudGod);
+        GameObject cl3 = Instantiate(cloudGod);
+        GameObject cl4 = Instantiate(cloudGod);
+        GameObject cl5 = Instantiate(cloudGod);
+        GameObject cl6 = Instantiate(cloudGod);
+        GameObject cl7 = Instantiate(cloudGod);
+        GameObject cl8 = Instantiate(cloudGod);
+        GameObject cl9 = Instantiate(cloudGod);
+        GameObject cl10 = Instantiate(cloudGod);
+        GameObject cl11 = Instantiate(cloudGod);
+        GameObject cl12 = Instantiate(cloudGod);
+        GameObject cl13 = Instantiate(cloudGod);
+        GameObject cl14 = Instantiate(cloudGod);
+        GameObject cl15 = Instantiate(cloudGod);
+        GameObject cl16 = Instantiate(cloudGod);
+        GameObject cl17 = Instantiate(cloudGod);
+        GameObject cl18 = Instantiate(cloudGod);
+        GameObject cl19 = Instantiate(cloudGod);
+        GameObject cl20 = Instantiate(cloudGod);
+
+        clouds.Add(cl1);
+        clouds.Add(cl2);
+        clouds.Add(cl3);
+        clouds.Add(cl4);
+        clouds.Add(cl5);
+        clouds.Add(cl6);
+        clouds.Add(cl7);
+        clouds.Add(cl8);
+        clouds.Add(cl9);
+        clouds.Add(cl10);
+        clouds.Add(cl11);
+        clouds.Add(cl12);
+        clouds.Add(cl13);
+        clouds.Add(cl14);
+        clouds.Add(cl15);
+        clouds.Add(cl16);
+        clouds.Add(cl17);
+        clouds.Add(cl18);
+        clouds.Add(cl19);
+        clouds.Add(cl20);
+
         timeCounter = 0;
         collCounter = 0;
+        cloudTimeCounter = 0;
+        cloudCounter = 0;
+        cloudSendNextTime = Random.Range(lowerCloudRangeSeconds, higherCloudRangeSeconds);
         sendNextTime = Random.Range(lowerRangeSeconds, higherRangeSeconds);
 
 
@@ -65,6 +120,32 @@ public class GameManagerScript : MonoBehaviour {
                 
             }
         }
+
+        //Cloud management software
+        if (cloudTimeCounter > cloudSendNextTime)
+        {
+            cloudTimeCounter = 0;
+            newStart = clouds[cloudCounter].transform.position;
+            clouds[cloudCounter].GetComponent<CloudScript>().rerollHeight();
+            clouds[cloudCounter].GetComponent<CloudScript>().rerollScale();
+
+            clouds[cloudCounter].transform.position = new Vector3(10, newStart.y, newStart.z);
+            cloudSendNextTime = Random.Range(lowerCloudRangeSeconds, higherCloudRangeSeconds);
+            //         print(collectibles[2].GetComponent<CollectibleScript>().speed);
+            //           print(collectibles[2].GetComponent<CollectibleScript>().higherRangeSpeed);
+
+            //        print(collectibles[2].GetComponent<CollectibleScript>().lowerRangeSpeed);
+            //       collectibles[1].GetComponent<CollectibleScript>().speed = Random.Range(GetComponent<CollectibleScript>().lowerRangeSpeed, GetComponent<CollectibleScript>().higherRangeSpeed);
+            clouds[cloudCounter].GetComponent<CloudScript>().isMoving = true;
+            cloudCounter++;
+            if (cloudCounter >= clouds.Count)
+            {
+                cloudCounter = 0;
+
+            }
+        }
+
+        cloudTimeCounter += Time.deltaTime;
         timeCounter += Time.deltaTime;
 	}
 }
