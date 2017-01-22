f6ec7476c32549708ef39902f5f16e08035cb25c
Sean Connolly <seanconnolly@mail.rit.edu>
2017-01-21 19:55:45 -0500

    update boatscript

--- WaveGame/Assets/Scripts/BoatScript.cs
+++ WaveGame/Assets/Scripts/BoatScript.cs
@@ -4,20 +4,14 @@ using UnityEngine;
 
 public class BoatScript : MonoBehaviour
 {
-    public WaterManager wave;
-    public float mass = 5;
-    public Vector2 initPos;
-    public Vector2 initVel;
-    public Vector2 initAccel;
-    public int nodeDifference = 1;
-    private Vector3 accel = new Vector2(0, 0);
-    private Vector3 vel = new Vector2(0, 0);
-    private Vector2 forceDir = new Vector2(1, 0);
-    private float forceMag = 500.0f;
-    private Vector2 force;
-    int index;
-    Vector2 lastPos;
+    
 
+    public WaterManager wave;
+    public int nodeDifference = 1;
+    public float lerpMultiplier = 7.0f;
+    private int index;
+    private Vector2 lastPos;
+    
     [SerializeField]
     float deathForce = .25f;
     [SerializeField]
@@ -59,10 +53,10 @@ public class BoatScript : MonoBehaviour
         Vector2 middleNode = wave.Positions[index];
         Vector2 nextNode = wave.Positions[index + nodeDifference];
         Vector2 prevNode = wave.Positions[index - nodeDifference];
-        transform.position = middleNode;
+        float yVal = Mathf.Lerp(transform.position.y, middleNode.y, Time.deltaTime* lerpMultiplier);
+        transform.position = new Vector2(middleNode.x, yVal);
         Vector2 diff = (nextNode - prevNode).normalized;
         transform.right = diff;
-        Vector2 offset = nextNode = middleNode;
 
         float angle = Mathf.Acos(Vector3.Dot(transform.right, Vector3.right)) * Mathf.Rad2Deg;
         float velocity = ((Vector2)transform.position - lastPos).magnitude;
