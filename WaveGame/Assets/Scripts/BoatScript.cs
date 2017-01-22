bfba8edaa76a3b5a59ea5b0ef498050682f5516a
Sean Connolly <seanconnolly@mail.rit.edu>
2017-01-21 20:29:22 -0500

    Editting Boat Script a bit.

--- WaveGame/Assets/Scripts/BoatScript.cs
+++ WaveGame/Assets/Scripts/BoatScript.cs
@@ -8,7 +8,8 @@ public class BoatScript : MonoBehaviour
 
     public WaterManager wave;
     public int nodeDifference = 1;
-    public float lerpMultiplier = 7.0f;
+    public float distMultiplier = 7.0f;
+    public float angleMultiplier = 7.0f;
     private int index;
     private Vector2 lastPos;
     
@@ -53,10 +54,11 @@ public class BoatScript : MonoBehaviour
         Vector2 middleNode = wave.Positions[index];
         Vector2 nextNode = wave.Positions[index + nodeDifference];
         Vector2 prevNode = wave.Positions[index - nodeDifference];
-        float yVal = Mathf.Lerp(transform.position.y, middleNode.y, Time.deltaTime* lerpMultiplier);
+        float yVal = Mathf.Lerp(transform.position.y, middleNode.y, Time.deltaTime* distMultiplier);
         transform.position = new Vector2(middleNode.x, yVal);
+
         Vector2 diff = (nextNode - prevNode).normalized;
-        transform.right = diff;
+        transform.right = Vector2.Lerp(transform.right, diff, Time.deltaTime*angleMultiplier);
 
         float angle = Mathf.Acos(Vector3.Dot(transform.right, Vector3.right)) * Mathf.Rad2Deg;
         float velocity = ((Vector2)transform.position - lastPos).magnitude;
